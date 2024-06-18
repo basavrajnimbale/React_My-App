7cb02cff433da8bb6cf34a1a5a33b878660ab56e
basavrajnimbale <nimbalebasavraj1@gmail.com>
2024-06-18 21:15:43 +0530

    complex jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
-function ExpenseItem(){
-    return <p>
-        Food - $5 <br/>
-        Petrol - $20 <br/>
-        Movie - $10
-    </p>
+function ExpenseItem() {
+    return (
+    <div>
+        <div>18 June 2024</div>
+        <div>Delhi</div>
+        <div>
+            <h2>Book</h2>
+            <div>$10</div>
+        </div>
+    </div>
+    );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
